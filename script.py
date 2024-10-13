1c23274ddd5e6b9e1b54356b0e135a0dd2c9c721
tomclaes <claes_tom@outlook.com>
2024-10-13 22:58:14 +0200

    led light script

--- script.py
+++ script.py
@@ -1,2 +1,27 @@
-print("hello world")
-print("hello world - 2")
+import subprocess
+import sys
+
+# Ensure the RPi.GPIO library is installed
+subprocess.check_call([sys.executable, "-m", "pip", "install", "RPi.GPIO"])
+
+import RPi.GPIO as GPIO
+import time
+
+# Set the GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# Set up GPIO pin 17 as an output
+LED_PIN = 17
+GPIO.setup(LED_PIN, GPIO.OUT)
+
+try:
+    # Turn on the LED
+    GPIO.output(LED_PIN, GPIO.HIGH)
+    print("LED is on")
+    
+    # Wait for 5 seconds
+    time.sleep(5)
+    
+    # Turn off the LED
+    GPIO.output(LED_PIN, GPIO.LOW)
+    print("LED is off")
